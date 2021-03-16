cbc67e48dd23de0488aba3f7465e892df1a7d10f
Toby <toby@tobymeehan.com>
2021-03-16 18:33:28 +0000

    Add data access for SQL
    Added dapper and interface layer for abstraction

---
+++ Api.Core/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Core.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> QueryAsync<T>(string sql, object parameters);
+
+        Task<int> ExecuteAsync(string sql, object parameters);
+    }
+}
---
+++ Api.Core/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Core.DataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly Func<IDbConnection> _connectionFactory;
+
+        public SqlDataAccess(Func<IDbConnection> connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public Task<int> ExecuteAsync(string sql, object parameters)
+        {
+            using (IDbConnection connection = _connectionFactory.Invoke())
+            {
+                return connection.ExecuteAsync(sql, parameters);
+            }
+        }
+
+        public Task<IEnumerable<T>> QueryAsync<T>(string sql, object parameters)
+        {
+            using (IDbConnection connection = _connectionFactory.Invoke())
+            {
+                return connection.QueryAsync<T>(sql, parameters);
+            }
+        }
+    }
+}
